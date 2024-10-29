2605fa5bb78d4ceee0ba4f29843a27105e95949e
abdul.rosyid [ITB-SOE] <abdul.rosyid@baf.id>
2024-10-29 16:28:54 +0700

    adjustmen main.go for vercel

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"go_decryptEnc/src/controller"
+	"net/http"
 )
 
-func main() {
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.POST("/encrypt-decrypt", controller.HandleEncryptDecrypt)
-	err := r.Run()
-	if err != nil {
-		return
-	}
+	return r
+}
+
+func VercelHandler(w http.ResponseWriter, r *http.Request) {
+	router := setupRouter()
+	router.ServeHTTP(w, r)
+}
+
+func main() {
+	router := setupRouter()
+	router.Run()
 }
